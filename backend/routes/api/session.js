5ed4cafcc36d405e98d1159a8a29475c6b3415de
celvenia <chriselvenia@gmail.com>
2023-01-11 18:29:26 -0700

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -37,6 +37,20 @@ router.delete(
     }
   );
 
+// Restore session user
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({ user: null });
+    }
+  );
+
   // if XSRF-Token isn't shown go here http://localhost:8000/api/csrf/restore
 
 module.exports = router;

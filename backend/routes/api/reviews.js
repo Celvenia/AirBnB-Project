5ffa65a12d26e04f01775cf699677b0f5738eaab
celvenia <chriselvenia@gmail.com>
2023-01-24 16:54:36 -0700

    added route to edit review by review id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,17 +1,49 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Review } = require('../../db/models');
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { Review } = require("../../db/models");
 
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
-router.get('/', async (req, res) => {
-    const reviews = await Review.findAll()
-    // console.log(reviews)
-    res.json(reviews)
-})
+const validateReview = [
+  check("review")
+    .exists({ checkFalsy: true })
+    .isString()
+    .withMessage("Review text is required"),
+  check("stars")
+    .exists({ checkFalsy: true })
+    .isInt({ min: 1, max: 5 })
+    .withMessage("stars needs to be an integer between 1 and 5"),
+  handleValidationErrors,
+];
 
+router.get("/", async (req, res) => {
+  const reviews = await Review.findAll();
+  // console.log(reviews)
+  res.json(reviews);
+});
+
+router.put("/:reviewId", requireAuth, validateReview, async (req, res) => {
+  const { review, stars } = req.body;
+  const reviewToEdit = await Review.findByPk(req.params.reviewId, {
+    where: {
+      userId: req.user.id,
+    },
+  });
+  if (!reviewToEdit) {
+    res.status(404).json({ message: "Review couldn't be found" });
+  } else if (reviewToEdit.userId != req.user.id) {
+    res.status(403).json({ message: "Forbidden" });
+  } else {
+    reviewToEdit.update({
+      review: review,
+      stars: stars,
+    });
+    await reviewToEdit.save();
+  }
+  return res.json(reviewToEdit);
+});
 
 module.exports = router;

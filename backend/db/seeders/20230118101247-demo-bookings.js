1d7cff1b78575d3e3dff96e78f25bd89321a1a2a
celvenia <chriselvenia@gmail.com>
2023-01-24 06:30:26 -0700

    added route to edit a booking by bookingId with authorized user

--- backend/db/seeders/20230118101247-demo-bookings.js
+++ backend/db/seeders/20230118101247-demo-bookings.js
@@ -14,32 +14,32 @@ module.exports = {
       {
         userId: 1,
         spotId: 1,
-        startDate: new Date(2023, 0, 20, 10, 30), //  Jan 03 2023 10:30 Within timeframe of booking
-        endDate: new Date(2023, 1, 4, 10, 30), //    Feb 04 2023 10:30
+        startDate: new Date(2023, 1, 25), //  Feb 25 2023
+        endDate: new Date(2023, 2, 4), //    March 04 2023 10:30 new Date(2023, 2, 4, 10, 30),
       },
       {
         userId: 2,
         spotId: 1,
-        startDate: new Date(2023, 5, 3, 10, 30), //  June 03 2023 10:30
-        endDate: new Date(2023, 5, 4, 10, 30), //    June 04 2023 10:30
+        startDate: new Date(2023, 5, 3), //  June 03 2023
+        endDate: new Date(2023, 5, 4), //    June 04 2023
       },
       {
         userId: 1,
         spotId: 1,
-        startDate: new Date(2023, 0, 3, 10, 30), //  Jan 03 2023 10:30
-        endDate: new Date(2023, 0, 4, 10, 30), //    Jan 04 2023 10:30 Past date
+        startDate: new Date(2023, 0, 3), //  Jan 03 2023
+        endDate: new Date(2023, 0, 4), //    Jan 04 2023 Past date
       },
       {
         userId: 1,
         spotId: 2,
-        startDate: new Date(2023, 7, 3, 10, 30), //  August 03 2023 10:30
-        endDate: new Date(2023, 7, 4, 10, 30), //    August 04 2023 10:30
+        startDate: new Date(2023, 7, 3), //  August 03 2023
+        endDate: new Date(2023, 7, 4), //    August 04 2023
       },
       {
         userId: 2,
         spotId: 2,
-        startDate: new Date(2023, 8, 3, 10, 30), //  September 03 2023 10:30
-        endDate: new Date(2023, 8, 4, 10, 30), //    September 04 2023 10:30
+        startDate: new Date(2023, 8, 3), //  September 03 2023
+        endDate: new Date(2023, 8, 4), //    September 04 2023
       }
     ], {});
   },
--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const moment = require("moment");
+
 const { setTokenCookie, requireAuth } = require("../../utils/auth");
 const { Booking, Owner } = require("../../db/models");
 
@@ -34,5 +36,82 @@ router.delete("/:bookingId", requireAuth, async (req, res) => {
 
   res.status(200).json({ message: "Successfully deleted" });
 });
+
+
+// edit booking by booking id with authorized user
+router.patch("/:bookingId", requireAuth, async (req, res) => {
+    const { startDate, endDate } = req.body;
+
+  const currentDate = new Date();
+
+  let dates = [];
+  let momentStart = moment(startDate);
+  let momentEnd = moment(endDate);
+  while (momentStart.isBefore(momentEnd) || momentStart.isSame(momentEnd)) {
+    dates.push(new Date(momentStart));
+    momentStart.add(1, "days");
+  }
+
+  const booking = await Booking.findByPk(req.params.bookingId, {
+    where: {
+      userId: req.user.id,
+    },
+  });
+
+  const spotBookings = await Booking.findAll({
+    where: {
+      spotId: booking.spotId,
+    },
+  });
+  let currentBookings = [];
+  spotBookings.forEach((spotBooking) => {
+    let momentStart = moment(spotBooking.startDate);
+    let momentEnd = moment(spotBooking.endDate);
+    while (momentStart.isBefore(momentEnd) || momentStart.isSame(momentEnd)) {
+      currentBookings.push(new Date(momentStart));
+      momentStart.add(1, "days");
+    }
+  });
+
+    if (!booking) {
+      res.status(404).json({ message: "Booking couldn't be found" });
+    } else if (booking.userId != req.user.id) {
+      res.status(401).json({ message: "Authentication required" });
+    } else if (startDate >= endDate) {
+      res
+        .status(400)
+        .json({
+          message: "Validation error",
+          statusCode: 400,
+          errors: ["endDate cannot come before startDate"],
+        });
+    } else if (booking.endDate <= currentDate) {
+      res.status(403).json({message: "Past bookings can't be modified"})
+    } else if (compareDates(dates, currentBookings)) {
+        res.status(403).json({message: "Sorry, this spot is already booked for the specified dates",
+    statusCode: 403,
+errors: [
+    "Start date conflicts with an existing booking",
+    "End date conflicts with an existing booking"
+]})
+    } else {
+      booking.update({
+        startDate: startDate,
+        endDate: endDate,
+      });
+      booking.save();
+    }
+  res.json(booking);
+});
+
+function compareDates(arr1, arr2) {
+    let set = new Set(arr2.map(date => date.getTime()));
+    for(let i = 0; i < arr1.length; i++) {
+        if(set.has(arr1[i].getTime())) {
+            return true
+        }
+    }
+    return false
+}
 
 module.exports = router;
